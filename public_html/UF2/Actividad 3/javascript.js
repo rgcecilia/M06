41e25faa8bab2ce19f3a2b0627416f344068b8ca
RaulGCecilia <rgcecilia@outlook.com>
2018-11-01 12:01:51 +0100

    First Commit

--- public_html/UF2/Actividad 3/javascript.js
+++ public_html/UF2/Actividad 3/javascript.js
@@ -1,7 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+var nombre;
+var url;
+
+class extra{
+    
+    
+
+  constructor(precio, url){
+    this.precio = precio;
+    this.url = url;
+  }
+  
+  getHTML(){
+      return this.nombre;
+      return this.url;
+  }
+     
+}
+lo
